015726120bfa8e5d48438c0c85c7c1eb93ca8e94
Jeong Min Lee <jacklee0223@gmail.com>
2019-12-05 22:03:05 -0600

    start skeleton template for navbar

---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>NavBar</div>;
+  }
+}
